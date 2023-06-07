b4deec6eb6f63b0256ae7de13783154eb376f000
seralol <79532085+seralol@users.noreply.github.com>
2023-06-07 22:30:31 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+BOT_TOKEN="6031401404:AAENXyVzAYQHNp74lXr-yXo72iDEk8P4b38"
+owner_id=5045853241
+log_group=-948372940
+orig_channel=-1001818894943
+
+bot_name = "DG_GAME_BOT"
+
+start_money = 100000
+cash_family = 0
+
+owner = '<a href="https://t.me/DeveIoper06">CHESTER</a>'
+channel = '<a href="t.me/DG_channnel">DG » Канал</a>'
+chat = '<a href="t.me/ChatikDg">DG » Чат</a>'
+chat_id=-1001931243692
